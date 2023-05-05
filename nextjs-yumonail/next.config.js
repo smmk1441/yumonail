39c867d384f3c6114eaed56d3f2aa2d9f8a2cefd
smmk1441 <55431228+smmk1441@users.noreply.github.com>
2023-05-05 18:39:58 +0000

    commit

---
+++ nextjs-yumonail/next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  compiler: {
+    // styledComponents の有効化
+    styledComponents: true,
+  },
+}
+module.exports = nextConfig
