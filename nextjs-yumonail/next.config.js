228d1511e0a53f061379d34f14bc33145b4566b6
smmk1441 <55431228+smmk1441@users.noreply.github.com>
2023-05-06 08:42:25 +0000

    commit

--- nextjs-yumonail/next.config.js
+++ nextjs-yumonail/next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    unoptimized: true,
+  },
   compiler: {
     // styledComponents の有効化
     styledComponents: true,
